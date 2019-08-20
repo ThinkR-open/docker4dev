303e5292bfc1b9ecb4d19e4000a746381984afe6
statnmap <statnmap@users.noreply.github.com>
2019-08-20 11:05:38 +0200

    install_server_docker et Readme

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # thinkr4servier
 
 Des trucs internes pour utiliser chez Servier mais qu'on garde pour nous
+
+## Docker comme chez Servier
+Utiliser le script `install_server_docker.R` dans son package. Ce script ne doit pas être envoyé sur git, ni être dans le package livré. 
+
+- Copier le fichier `install_server_docker.R` à la racine de votre projet
+- Cacher de git et build
+```r
+usethis::use_git_ignore("install_server_docker.R")
+usethis::use_build_ignore("install_server_docker.R")
+```
+
+- Exécuter la partie `firsttime` à la première utilisation pour créer le Docker
+- Les fois suivantes, il suffit de le lancer directement
+
+> Les packages installés en cours de développement sont persistents dans votre projet (caché de git et de build aussi)
+
---
+++ install_server_docker.R
@@ -0,0 +1,46 @@
+## Allow us to lanch the system command in a new R session
+library(future)
+plan(multisession)
+
+### Set good working directory for docker volumes
+my_project <- here::here()
+my_library <- here::here("library")
+
+# First creation of the package
+firsttime <- FALSE
+if (firsttime) {
+  ## To pass the check
+  usethis::use_git_ignore("library/")
+  usethis::use_build_ignore("library/")
+  usethis::use_build_ignore("install_server.R")
+  usethis::use_build_ignore("script_for_package.R")
+  usethis::use_build_ignore("Dockerfile")
+  
+  ## Build the image with the Dockerfile if necessary
+  system("docker build -t rstudio3_5_2 .", intern = TRUE)
+  # Send to Docker hub
+  # docker login --username=yourhubusername
+  # docker images 
+  # Find the latest build
+  # docker tag 27cebcec37e1 yourhubusername/rstudio3_5_2:v0.2
+  # docker push yourhubusername/rstudio3_5_2:v0.2
+  # Or use docker pull thinkr/rstudio3_5_2
+}
+
+## Launch the server in the new R session (terminal have to be active...)
+future({
+  system(paste0("docker run --name serveraegen -v ",my_library,":/home/rstudio/library -v ",my_project,":/home/rstudio/aegen  -p 127.0.0.1:8787:8787 -e DISABLE_AUTH=true   rstudio3_5_2"), intern = TRUE)
+})
+
+# _You will need to set the credentials
+# git config credential.helper store
+# _and your name
+# usethis::use_git_config(scope = "project", user.name = "", user.email = "@thinkr.fr")
+
+Sys.sleep(2)
+browseURL("http://127.0.0.1:8787")
+
+## To stop your image after your work proprely
+system("docker kill serveraegen")
+system("docker rm serveraegen")
+
