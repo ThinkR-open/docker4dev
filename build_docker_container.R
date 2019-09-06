fb1094623031164a0f3e45e092f6aaabbeced41b
statnmap <statnmap@users.noreply.github.com>
2019-09-06 23:48:08 +0200

    dockerfiles with downgrade packages

--- build_docker_container.R
+++ build_docker_container.R
@@ -1,12 +1,18 @@
 # Creation of the Docker container
 ## Build the image with the Dockerfile if necessary
-system("docker build -t rstudio3_5_2 Dockerfile_R-3.5.2-rocker-verse", intern = TRUE)
-system("docker build -t rstudio3_5_2_geo Dockerfile_R-3.5.2-rocker-verse-geospatial", intern = TRUE)
+system("docker build -t rstudio3_5_2 Dockerfile_R-3.5.2-rocker-verse", intern = FALSE)
+system("docker build -t rstudio3_5_2_geo Dockerfile_R-3.5.2-rocker-verse-geospatial", intern = FALSE)
 
-# Send to Docker hub
-# docker login --username=yourhubusername
+## Send to Docker hub
+# docker login --username=thinkr
 # docker images 
-# Find the latest build
-# docker tag 27cebcec37e1 yourhubusername/rstudio3_5_2:v0.2
-# docker push yourhubusername/rstudio3_5_2:v0.2
+## _Find the latest build of your image
+# docker tag 518a41981a6a myRegistry.com/myImage
+# ou
+# docker tag rstudio3_5_2 thinkr/rstudio3_5_2:v0.1
+## _Push
+# docker push thinkr/rstudio3_5_2:v0.1
+## Get
 # Or use docker pull thinkr/rstudio3_5_2
+# docker pull thinkr/rstudio3_5_2_geo
+
